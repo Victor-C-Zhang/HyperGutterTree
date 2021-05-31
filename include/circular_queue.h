5b93bbcb8a732f7504c508c5af8b83a58465cafe
Evan West <evtwest@yahoo.com>
2021-05-30 21:35:32 -0400

    fix typo in comment

--- include/circular_queue.h
+++ include/circular_queue.h
@@ -14,8 +14,8 @@ struct queue_elm {
 /*
  * A circular queue of data elements.
  * Used in the bufferTree to place leaf data which is ready to be processed.
- * Has a finite size and will block operations which do not have the data
- * that they need (either empty or full for peek and push respectively)
+ * Has a finite size and will block operations which do not have what they
+ * need need (either empty or full for peek and push respectively)
  */
 
 class CircularQueue {
