31fd4d91d65131bea9707c7d1066446b5c8f65fe
Victor Zhang <vcz4@scarletmail.rutgers.edu>
2021-11-09 16:44:55 -0500

    refactor: node_id_t from common repo

--- include/types.h
+++ include/types.h
@@ -1,7 +1,8 @@
 #pragma once
 #include <utility>
 #include <vector>
-typedef uint32_t node_id_t;
+#include <graph_zeppelin_common.h>
+
 typedef std::pair<node_id_t, node_id_t> update_t;
 typedef void insert_ret_t;
 typedef void flush_ret_t;
