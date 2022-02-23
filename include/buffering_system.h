bd1406ffeee20f7dcbbdd66f7bfc9a17a3215b26
Evan West <evtwest@yahoo.com>
2022-02-23 12:55:24 -0500

    refactor some code to a cpp file to reduce side of header file

--- include/buffering_system.h
+++ include/buffering_system.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <cstdint>
 #include "types.h"
 #include <math.h>
 
--- include/cache_gutter_tree.h
+++ include/cache_gutter_tree.h
@@ -87,40 +87,8 @@ class CacheGutterTree : public BufferingSystem {
       }
     }
 
-    void flush(bool force = false) {
-      if (leafNode) {
-        for (const auto &update : buffer) {
-          auto &outputBuffer = outputBuffers[update.first - updateSpan.first];
-          outputBuffer.push_back(update.second);
-          if (outputBuffer.size() == config.bufferSize) {
-            int data_size = config.bufferSize * sizeof(node_id_t);
-            config.wq.push(reinterpret_cast<char *>(outputBuffer.data()), data_size);
-            outputBuffer.clear();
-            outputBuffer.push_back(updateSpan.first + update.first);
-          }
-        }
-        if (force) {
-          const size_t numOutputBuffers = updateSpan.second - updateSpan.first + 1;
-          for (size_t i = 0; i < numOutputBuffers; ++i) {
-            std::vector<node_id_t> buffer = outputBuffers[i];
-            if (buffer.size() > 0) {
-              int data_size = buffer.size() * sizeof(node_id_t);
-              config.wq.push(reinterpret_cast<char *>(buffer.data()), data_size);
-              buffer.clear();
-              buffer.push_back(updateSpan.first + i);
-            }
-          }      
-        }
-      } else {
-        const size_t spanQuanta = calculateSpanQuanta(updateSpan);
-        for (const auto &update : buffer) {
-          const size_t bucket = std::min(static_cast<size_t>((update.first - updateSpan.first) 
-                                / spanQuanta), numChildren - 1);
-          childNodes[bucket].insert(update);
-        }
-      }
-      buffer.clear();
-    }
+    void flush(bool force = false);
+    
     // flush this node and all its children of any updates
     void flush_tree() {
       flush(true);
@@ -161,33 +129,7 @@ class CacheGutterTree : public BufferingSystem {
    * @param data        to store the fetched data.
    * @return            true if got valid data, false if unable to get data.
    */
-  bool get_data(data_ret_t &data) {
-    // make a request to the circular buffer for data
-    std::pair<int, queue_elm> queue_data;
-    bool got_data = wq.peek(queue_data);
-
-    if (!got_data) return false;  // we got no data so return not valid
-    int i = queue_data.first;
-    queue_elm elm = queue_data.second;
-    auto *serial_data = reinterpret_cast<node_id_t *>(elm.data);
-    uint32_t len = elm.size;
-    assert(len % sizeof(node_id_t) == 0);
-
-    if (len == 0) return false;  // we got no data so return not valid
-    // assume the first key is correct so extract it
-    node_id_t key = serial_data[0];
-    data.first = key;
-
-    data.second.clear();  // remove any old data from the vector
-    uint32_t vec_len = len / sizeof(node_id_t);
-    data.second.reserve(vec_len);  // reserve space for our updates
-    for (uint32_t j = 1; j < vec_len; ++j) {
-      data.second.push_back(serial_data[j]);
-    }
-
-    wq.pop(i);  // mark the wq entry as clean
-    return true;
-  }
+  bool get_data(data_ret_t &data);
 
   /**
    * Flushes all pending buffers.
@@ -203,12 +145,5 @@ class CacheGutterTree : public BufferingSystem {
    * @param    block is true if we should turn on non-blocking operations
    *           and false if we should turn them off.
    */
-  void set_non_block(bool block) {
-    if (block) {
-      wq.no_block = true;  // circular queue operations should no longer block
-      wq.wq_empty.notify_all();
-    } else {
-      wq.no_block = false;  // set circular queue to block if necessary
-    }
-  }
+  void set_non_block(bool block);
 };
