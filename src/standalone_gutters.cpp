c9fcdee997c90feec909fca925fe4bf4f3a3478a
daniel delayo <dandelayo@gmail.com>
2022-04-08 18:36:48 +0000

    Assume force_flush is only called when the inserter threads are paused

--- src/standalone_gutters.cpp
+++ src/standalone_gutters.cpp
@@ -17,7 +17,7 @@ StandAloneGutters::StandAloneGutters(node_id_t num_nodes, uint32_t workers, uint
 
 insert_ret_t StandAloneGutters::insert(const update_t &upd, int which) {
   Gutter &gutter = local_buffers[which][upd.first];
-  const std::lock_guard<std::mutex> lock(gutter.mux);
+  //const std::lock_guard<std::mutex> lock(gutter.mux);
   std::vector<node_id_t> &ptr = gutter.buffer;
   ptr.push_back(upd.second);
   if (ptr.size() == local_buf_size) { // full, so request flush
@@ -53,7 +53,7 @@ flush_ret_t StandAloneGutters::force_flush() {
     const std::lock_guard<std::mutex> lock(gutters[node_idx].mux);
     for (uint32_t which = 0; which < inserters; which++)
     {
-      const std::lock_guard<std::mutex> lock(local_buffers[which][node_idx].mux);
+      //const std::lock_guard<std::mutex> lock(local_buffers[which][node_idx].mux);
       insert_batch(which, node_idx);
     }
     if (!gutters[node_idx].buffer.empty()) { // have stuff to flush
